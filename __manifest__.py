ef99b5ebd7d448f4cf354517181e9fc0fc8b192c
Romke Jonker <romke@jnkr.nl>
2024-06-27 11:33:54 +0200

    Add stimula-odoo module

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# __manifest__.py
+{
+    'name': 'Stimula for Odoo',
+    'version': '1.0',
+    'summary': 'Adds Simple Table Mapping Language (STML) API to Odoo',
+    'author': 'Romke Jonker',
+    'website': 'https://www.stml.io',
+    'category': 'Custom',
+    'depends': ['base'],
+    'external_dependencies': {
+    },
+    'data': [
+        # List of XML or CSV files to be loaded
+        # 'controllers/main.py'
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
