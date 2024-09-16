65cd8cde16973906758c755f44d58acd9c8cc2e5
Romke Jonker <romke@jnkr.nl>
2024-09-16 12:00:14 +0200

    Add trackable link for module info

--- stimula_odoo/__manifest__.py
+++ stimula_odoo/__manifest__.py
@@ -4,11 +4,11 @@
     'version': '1.0',
     'summary': 'Adds Simple Table Mapping Language (STML) API to Odoo',
     'author': 'Romke Jonker',
-    'website': 'https://www.stml.io',
+    'website': 'https://www.stml.io/odoo_module',
     'category': 'Custom',
     'depends': ['base'],
     'external_dependencies': {
-        'python': ['stimula>=0.0.4'],
+        'python': ['stimula>=0.0.17'],
     },
     'data': [
         # List of XML or CSV files to be loaded
